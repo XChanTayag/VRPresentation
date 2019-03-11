506cd18223f8683edb6d07e21730e970be501d45
Alexx06 <alextrod06@gmail.com>
2019-03-11 10:23:17 +0800

    add audio and models

--- Assets/Scripts/Transitions/ShowObjects.cs
+++ Assets/Scripts/Transitions/ShowObjects.cs
@@ -1,16 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRStandardAssets.Utils;
+using System;
+using UnityEngine.UI;
 
-public class ShowObjects : MonoBehaviour {
+//[System.Serializable]
+//public class SlidesModels
+//{
+//    public GameObject[] objects;
+//}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class ShowObjects : MonoBehaviour
+{
+
+    [SerializeField] private SelectionSlider m_SelectionSlider;             // Used to activate function
+    [SerializeField] private string SceneToLoad = "Phyla";                  // The scene to load default is Phyla
+
+    public float timer = Time.time;
+    public float timeElapsed = 0;
+    //public SlidesModels[] showObjects;
+    private bool flag = true;
+
+    void Start()
+    {
+        flag = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag == false)
+        {
+            SetLocation();
+        }
+    }
+    private void OnEnable()
+    {
+        m_SelectionSlider.OnBarFilled += HandleBarFilled;
+    }
+
+
+    private void OnDisable()
+    {
+        m_SelectionSlider.OnBarFilled -= HandleBarFilled;
+    }
+
+    private void HandleBarFilled()
+    {
+
+        ResetTime();
+        flag = false;
+        
+        
+    }
+    private void ResetTime()
+    {
+        timer = Time.time;
+        float timereseted = timer - timeElapsed;
+        Debug.Log(timereseted.ToString());
+        timeElapsed = timer;
+    }
+    private void SetLocation()
+    {
+        //float locationX = showObjects.transform.position.x;
+        //float locationY = showObjects.transform.position.y + 0.05f;
+        //float locationZ = showObjects.transform.position.z;
+        //showObjects.transform.position = new Vector3(locationX, locationY, locationZ);
+        //if (locationY > 2)
+        //{
+        //    flag = true;
+        //}
+    }
 }
--- Assets/SlideLoad.cs
+++ Assets/SlideLoad.cs
@@ -6,21 +6,59 @@ using VRStandardAssets.Utils;
 using System;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class SlidesModels
+{
+    public GameObject[] objects;
+    public int[] timeShow;
+}
+
 public class SlideLoad : MonoBehaviour
 {
 
     [SerializeField] private SelectionSlider m_SelectionSlider;             // Used to activate function
     [SerializeField] private string SceneToLoad = "Phyla";                  // The scene to load default is Phyla
     public Texture2D[] imageNum = new Texture2D[5];
+    public AudioSource audiosource;
+    public AudioClip[] audioSlides;
+
     public RawImage slideImage;
     public int count;
     public Text textyes;
+
+
+    public int numberOfObjects;
+
+    public float timereseted;
+    public float timer = Time.time;
+    public float timeElapsed = 0;
+    public int sec;
+    private float temp;
+
+
+    public SlidesModels[] listShowtime;
+
+    private bool flag = true;
+
+    void Start()
+    {
+        timer = Time.time;
+        flag = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeUpdate();
+        SetLocation();
+    }
+
+
     private void OnEnable()
     {
         m_SelectionSlider.OnBarFilled += HandleBarFilled;
     }
-
-
     private void OnDisable()
     {
         m_SelectionSlider.OnBarFilled -= HandleBarFilled;
@@ -28,18 +66,39 @@ public class SlideLoad : MonoBehaviour
 
     private void HandleBarFilled()
     {
+        int a;
+        int b;
+        try
+        {
+            a = listShowtime.Length;
+            b = listShowtime[count].timeShow.Length;
+        }
+        catch (Exception ex)
+        {
+            a = listShowtime.Length;
+            b = 11111;
+        }
+        Debug.Log(a + " " + b);
+        SlideDetector();
+        ResetTime();
+        flag = false;
+    }
+    private void SlideDetector()
+    {
         Debug.Log(textyes.text);
+        Debug.Log(count.ToString());
 
         while (true)
         {
             if (slideImage.texture == imageNum[count])
             {
                 Debug.Log("found");
+                HideModels();
                 break;
             }
             count++;
         }
-        
+
         try
         {
             if (textyes.text == "Next")
@@ -59,12 +118,84 @@ public class SlideLoad : MonoBehaviour
                 }
 
             }
+            PlayAudio();
         }
         catch (Exception ex)
         {
-            count--;
-            slideImage.texture = imageNum[count];
+            
         }
         Debug.Log(count.ToString());
+    }
+
+    private void ResetTime()
+    {
+        timer = Time.time;
+        timereseted = timer - timeElapsed;
+        Debug.Log(timereseted.ToString());
+        timeElapsed = timer;
+        sec = 0;
+    }
+    private void TimeUpdate()
+    {
+        timer = Time.time;
+        float seconds = timer % 60;
+        if ((int)temp < (int)seconds)
+        {
+            sec++;
+            temp = seconds;
+        }
+    }
+    private void PlayAudio()
+    {
+        audiosource.clip = audioSlides[count];
+        audiosource.Play();
+
+    }
+    private void SetLocation()
+    {
+        try
+        {
+            int a = listShowtime.Length;
+            int b = listShowtime[count].timeShow.Length;
+
+            for (int i = 0; i < b; i++)
+            {
+                if (listShowtime[count].timeShow[i] <= sec)
+                {
+                    float locationX = listShowtime[count].objects[i].transform.position.x;
+                    float locationY = listShowtime[count].objects[i].transform.position.y;
+                    float locationZ = listShowtime[count].objects[i].transform.position.z;
+                    if (locationY < 2)
+                    {
+                        locationY = listShowtime[count].objects[i].transform.position.y + 0.05f;
+                        listShowtime[count].objects[i].transform.position = new Vector3(locationX, locationY, locationZ);
+
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+    private void HideModels()
+    {
+        try
+        {
+            int a = listShowtime.Length;
+            int b = listShowtime[count].timeShow.Length;
+
+            for (int i = 0; i < b; i++)
+            {
+                float locationX = listShowtime[count].objects[i].transform.position.x;
+                float locationZ = listShowtime[count].objects[i].transform.position.z;
+                listShowtime[count].objects[i].transform.position = new Vector3(locationX, -5f, locationZ);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
     }
 }
